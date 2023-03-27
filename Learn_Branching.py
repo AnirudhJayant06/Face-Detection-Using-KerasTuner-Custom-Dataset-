67fe76a7f0fc38abf105dbac33f78bbefd3dc738
AnirudhJayant06 <anirudh.jayant@gmail.com>
2023-03-27 17:42:24 +0530

    Adding conflict

--- Learn_Branching.py
+++ Learn_Branching.py
@@ -5,5 +5,5 @@ def Check_Branching_and_Merge():
     print('Cheking Branching & Merging')
 
 def Learning_merge_conflict():
-    print('I am creating conflict')
+    print('Learning merge conflict ..Updating the comment')
 
