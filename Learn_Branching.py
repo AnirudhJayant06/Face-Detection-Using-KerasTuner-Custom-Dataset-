6754acd65728ae9ae0497b24a9c1fbdcd1b625fe
AnirudhJayant06 <anirudh.jayant@gmail.com>
2023-03-27 16:50:21 +0530

    Adding another method Learning_merge_conflict

--- Learn_Branching.py
+++ Learn_Branching.py
@@ -3,3 +3,6 @@ print('I am creating a new branch')
 
 def Check_Branching_and_Merge():
     print('Cheking Branching & Merging')
+
+def Learning_merge_conflict():
+    print('Learning merge conflict')
